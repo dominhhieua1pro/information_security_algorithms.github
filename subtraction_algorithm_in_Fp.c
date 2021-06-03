3f91adc7a69b429100e5e16a734e6ac491680241
Đỗ Minh Hiếu <dominhhieua1pro@gmail.com>
2021-06-03 18:44:04 +0700

    Do Minh Hieu

--- subtraction_algorithm_in_Fp.c
+++ subtraction_algorithm_in_Fp.c
@@ -1,26 +1,22 @@
-#include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
-#include<conio.h>
 
 int w, m, t, i, e, select;
 unsigned int a, b , p, c;
 static int A[50], B[50], C[50], P[50];
 
-void input(int A[]){
-    for(i = 0; i < t; i++){
-        scanf("%d", &A[t - i - 1]);
-    }
-}
 void convert_integer_to_array(unsigned int a, int A[], int w, unsigned int p){
 	m = (int)(log(p) / log(2)) + 1;
-	t = (int)(m / w) + 1;
+	if(m % w != 0)    t = (int)(m / w) + 1;
+	else    t = (int)(m / w);
 	for(i = 0; i < t ; i++){
 		A[t-i-1] = (int) (a / pow(2,(t-i-1)*w));
 		a = a - A[t-i-1]*(pow(2,(t-i-1)*w));
 	}
 }
 void multiprecision_subtraction(){
+    convert_integer_to_array(a, A, w, p);
+    convert_integer_to_array(b, B, w, p);
 	e = 0;
 	for(i = 0; i < t; i++){
 		C[i] = A[i] - B[i] - e;
@@ -61,51 +57,13 @@ void display(){
 	printf("\n\ne = %d\n", e);
 }
 int main(){
-	while(1){
-		system("cls");
-		system("color f0");
-		printf("Subtraction algorithm in the Fp field!\n");
-		printf("\nChon cach nhap input:\n");
-		printf("1. Integer\n");
-		printf("2. Array\n");
-		printf("Enter selection: ");
-		scanf("%d",&select);
-		switch(select){
-			case 1:
-				printf("\nNhap input tu 2 so nguyen a, b\n");
-				printf("\nEnter a, b: ");
-				scanf("%d %d", &a, &b);
-				printf("\nEnter p, w: ");
-				scanf("%d %d", &p, &w);
-				convert_integer_to_array(a, A, w, p);
-				convert_integer_to_array(b, B, w, p);
-				multiprecision_subtraction();
-				subtraction_algorithm();
-				display();
-				printf("\nPress any key to return.");
-				getch();
-				break;
-			case 2:
-				printf("\nNhap input tu 2 mang A, B\n");
-				printf("\nEnter p, w: ");
-				scanf("%d %d", &p, &w);
-				m = (int)(log(p) / log(2)) + 1;
-				t = (int)(m / w) + 1;
-				printf("\nNhap mang A: ");
-				input(A);
-				printf("\nNhap mang B: ");
-				input(B);
-				multiprecision_subtraction();
-				subtraction_algorithm();
-				display();
-				printf("\nPress any key to return.");
-				getch();
-				break;
-			default:
-				printf("Nhap khong hop le!");
-				printf("\nPress any key to return.");
-				getch();
-		}
-	}
-	return 0;
+    printf("Subtraction algorithm in the Fp field!\n");
+    printf("\nEnter a, b: ");
+    scanf("%d %d", &a, &b);
+    printf("\nEnter p, w: ");
+    scanf("%d %d", &p, &w);
+    multiprecision_subtraction();
+    subtraction_algorithm();
+    display();
+    return 0;
 }
